3a64df05d30596de7abe280718bfd216ce74a1ee
Mauricio Leonardo Alemán Díaz <mauricio.leonardo.aleman.diaz1@gmail.com>
2023-10-01 12:54:26 -0600

    creacion de moviemiento y animacion de jugador

--- jugador/Jugador.gd
+++ jugador/Jugador.gd
@@ -6,6 +6,11 @@ const friccion = 500
 
 var velocidad = Vector2.ZERO
 
+onready var animacionJugador = $AnimationPlayer
+onready var arbolAnimacion = $AnimationTree
+onready var estadoAnimacion = arbolAnimacion.get("parameters/playback")
+
+
 func _physics_process(delta):
 	
 	var input_vector = Vector2.ZERO
@@ -14,9 +19,14 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		
+		arbolAnimacion.set("parameters/Estatico/blend_position",input_vector)
+		arbolAnimacion.set("parameters/Correr/blend_position",input_vector)
+		estadoAnimacion.travel("Correr")
 		velocidad += input_vector * aceleracion * delta
 		velocidad = velocidad.clamped(velocidadMaxima * delta)
 	else:
+		estadoAnimacion.travel("Estatico")
 		velocidad = velocidad.move_toward(Vector2.ZERO,friccion * delta)
 		
 	
