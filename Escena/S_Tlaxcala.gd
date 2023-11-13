eec0eb597fb0df4da26a985f5db612bbd46c5a24
IustusRenidet <143914334+MauricioAlemanD@users.noreply.github.com>
2023-11-13 07:41:30 -0600

    Vadenuez_Tlaxcala

--- Escena/S_Tlaxcala.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 6a8c11c9aac1699f6c309cf24a9a9da64ae702ea
-func _ready():
-	UsuarioGlobal.nivelActual = "Tlaxcala"
-	$Jugador/Camera2D/HUD/lblNivel.text = "Nivel Tlaxcala"
