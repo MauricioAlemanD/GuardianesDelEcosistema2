4c1a51112c1030069063fe57117e1c2fd315e81b
IustusRenidet <143914334+MauricioAlemanD@users.noreply.github.com>
2023-11-12 23:32:57 -0600

    a

--- Escena/S_Tlaxcala.gd
+++ Escena/S_Tlaxcala.gd
@@ -1,4 +1,9 @@
 extends Node2D
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 6a8c11c9aac1699f6c309cf24a9a9da64ae702ea
 func _ready():
 	UsuarioGlobal.nivelActual = "Tlaxcala"
 	$Jugador/Camera2D/HUD/lblNivel.text = "Nivel Tlaxcala"
