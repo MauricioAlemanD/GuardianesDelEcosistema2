ab359616528cdcbead194c4aa3745a4dcb7d6fd4
IustusRenidet <143914334+MauricioAlemanD@users.noreply.github.com>
2023-12-12 20:18:04 -0600

    guardarpos

--- Escena/Subterraneo.gd
+++ Escena/Subterraneo.gd
@@ -1,2 +1,9 @@
 extends Node2D
-var outside=""
+var entered=false
+var outside="res://Escena/S_CDMX.tscn"
+
+func _on_exit_body_entered(body):
+		get_tree().change_scene(outside)
+	
+func _on_exit_body_exited(body):
+	pass
