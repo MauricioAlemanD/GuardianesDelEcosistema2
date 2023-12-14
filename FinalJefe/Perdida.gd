3059023b78eb548cd8f422e93651f8506c441889
Mauricio Leonardo Alemán Díaz <mauricio.leonardo.aleman.diaz1@gmail.com>
2023-12-14 10:29:19 -0600

    escena perdida

---
+++ FinalJefe/Perdida.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var textoCompleto = "PERDISTE"
+var textoActual = ""
+var indiceTexto = 0
+
+func _ready():
+	# Configura el temporizador
+	$tiempoDialogo.wait_time = 0.06
+	$tiempoDialogo.one_shot = false
+	$tiempoDialogo.connect("timeout", self, "_on_temporizador_timeout")
+	$tiempoDialogo.start()
+	$btnContinuar.visible = false
+	
+func _on_temporizador_timeout():
+	if indiceTexto < textoCompleto.length():
+		# Añade un carácter al texto actual
+		textoActual += textoCompleto[indiceTexto]
+		$Dialogo.text = textoActual
+		indiceTexto += 1
+	else:
+		$tiempoDialogo.stop()
+		$btnContinuar.visible = true
+
+
+func _on_Button_pressed():
+	UsuarioGlobal.nivelActual = "Morelos"
+	UsuarioGlobal.guardar_progreso()
+	get_tree().change_scene("res://Escena/seleccionNiveles.tscn")
